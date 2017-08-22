48befd839887b0ffae420522284c1749a18916b2
Phon Phey <phonphey@o2.pl>
2017-08-22 12:55:11 +0300

    bn

--- util.py
+++ util.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2017 phonphey
+# 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from numpy import random as nprand
 from cassandra.cluster import Cluster
 
