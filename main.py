3b22ef79279fd541c25f4601fbd67fc729fc145d
oksovskii <21ololoha@gmail.com>
2017-08-24 16:27:26 +0300

    fix

--- main.py
+++ main.py
@@ -20,12 +20,17 @@ def main():
     elif type == "blackhole":
         gen.gen_blackhole(int(sys.argv[2]), float(sys.argv[3]))     
     
+    elif type == "nebula":
+        gen.gen_nebula(int(sys.argv[2]), float(sys.argv[3]))   
+    
     elif type == "all":
         gen.gen_stars(int(sys.argv[2]), float(sys.argv[3]))
         gen.gen_comets(int(sys.argv[2]), float(sys.argv[3]))
         gen.gen_planets(int(sys.argv[2]), float(sys.argv[3]))
         gen.gen_meteorites(int(sys.argv[2]), float(sys.argv[3]))
         gen.gen_blackhole(int(sys.argv[2]), float(sys.argv[3])) 
+        gen.gen_nebula(int(sys.argv[2]), float(sys.argv[3]))
+    
     
     else:
         print("%s type is not defined" % type)
